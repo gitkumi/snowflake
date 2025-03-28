6912e578fd73cd880c99a5f7b33f1c563b4ef4ab
Takumi Ito <ito.tkmi@gmail.com>
2025-03-28 21:08:52 +0900

    Create wrapper funcs

--- cmd/root.go
+++ cmd/root.go
@@ -59,114 +59,116 @@ func new() *cobra.Command {
 		Short: "Create a new project",
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			projectName := args[0]
-
 			cwd, err := os.Getwd()
 			if err != nil {
 				log.Fatal(err.Error())
 			}
 
-			project := &Project{
-				Name: strings.ToLower(projectName),
-			}
-
-			outputPath := filepath.Join(cwd, projectName)
-
-			templateFiles := snowflaketemplate.ApiFiles
-
-			err = fs.WalkDir(templateFiles, ".", func(path string, d fs.DirEntry, err error) error {
-				if err != nil {
-					return err
-				}
-
-				fileName := strings.TrimPrefix(path, "api")
-
-				if d.IsDir() {
-					err := os.MkdirAll(filepath.Join(outputPath, fileName), 0777)
-					return err
-				}
-
-				content, err := templateFiles.ReadFile(path)
-				if err != nil {
-					return err
-				}
-
-				tmpl, err := template.New(fileName).Parse(string(content))
-				if err != nil {
-					return err
-				}
-
-				var buf bytes.Buffer
-				err = tmpl.Execute(&buf, project)
-				if err != nil {
-					return err
-				}
-
-				newFilePath := strings.TrimSuffix(filepath.Join(outputPath, fileName), ".templ")
-				err = os.WriteFile(newFilePath, buf.Bytes(), 0777)
-				return err
-			})
-
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-
-			command := exec.Command("go", "mod", "init", project.Name)
-			command.Dir = outputPath
-			err = command.Run()
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-
-			command = exec.Command("go", "mod", "tidy")
-			command.Dir = outputPath
-			err = command.Run()
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-
-			command = exec.Command("gofmt", "-w", "-s", ".")
-			command.Dir = outputPath
-			err = command.Run()
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-
-			command = exec.Command("make", "build")
-			command.Dir = outputPath
-			err = command.Run()
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-
-			if initGit {
-				command = exec.Command("git", "init")
-				command.Dir = outputPath
-				err = command.Run()
-				if err != nil {
-					log.Fatal(err.Error())
-				}
-
-				command = exec.Command("git", "add", "-A")
-				command.Dir = outputPath
-				err = command.Run()
-				if err != nil {
-					log.Fatal(err.Error())
-				}
-
-				command = exec.Command("git", "commit", "-m", "Initialize Snowflake project")
-				command.Dir = outputPath
-				err = command.Run()
-				if err != nil {
-					log.Fatal(err.Error())
-				}
-			}
-
-			fmt.Println("Initialized Snowflake project.")
+			CreateProject(args[0], initGit, cwd)
 		},
 	}
 
 	cmd.Flags().BoolVarP(&initGit, "git", "g", true, "Initialize git")
 
 	return cmd
+}
+
+func CreateProject(projectName string, initGit bool, outputDir string) {
+	project := &Project{
+		Name: strings.ToLower(projectName),
+	}
+
+	outputPath := filepath.Join(outputDir, projectName)
+
+	templateFiles := snowflaketemplate.ApiFiles
+
+	err := fs.WalkDir(templateFiles, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		fileName := strings.TrimPrefix(path, "api")
+
+		if d.IsDir() {
+			err := os.MkdirAll(filepath.Join(outputPath, fileName), 0777)
+			return err
+		}
+
+		content, err := templateFiles.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		tmpl, err := template.New(fileName).Parse(string(content))
+		if err != nil {
+			return err
+		}
+
+		var buf bytes.Buffer
+		err = tmpl.Execute(&buf, project)
+		if err != nil {
+			return err
+		}
+
+		newFilePath := strings.TrimSuffix(filepath.Join(outputPath, fileName), ".templ")
+		err = os.WriteFile(newFilePath, buf.Bytes(), 0777)
+		return err
+	})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	command := exec.Command("go", "mod", "init", project.Name)
+	command.Dir = outputPath
+	err = command.Run()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	command = exec.Command("go", "mod", "tidy")
+	command.Dir = outputPath
+	err = command.Run()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	command = exec.Command("gofmt", "-w", "-s", ".")
+	command.Dir = outputPath
+	err = command.Run()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	command = exec.Command("make", "build")
+	command.Dir = outputPath
+	err = command.Run()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if initGit {
+		command = exec.Command("git", "init")
+		command.Dir = outputPath
+		err = command.Run()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		command = exec.Command("git", "add", "-A")
+		command.Dir = outputPath
+		err = command.Run()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		command = exec.Command("git", "commit", "-m", "Initialize Snowflake project")
+		command.Dir = outputPath
+		err = command.Run()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+
+	fmt.Println("Done!")
 }
