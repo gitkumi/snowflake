ffa28c798e53dce8f13b5e7471e1c71fb6fc5a6f
Takumi Ito <ito.tkmi@gmail.com>
2025-04-05 20:11:35 +0900

    Add file rename

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sort"
 	"strings"
 	"text/template"
 
@@ -31,7 +32,7 @@ type FileExclusions struct {
 	ByAppType map[AppType][]string
 }
 
-type FileRenaming struct {
+type FileRenames struct {
 	ByAppType map[AppType]map[string]string
 }
 
@@ -47,7 +48,7 @@ func Generate(cfg *GeneratorConfig) error {
 
 	templateFuncs := createTemplateFuncs(cfg)
 	exclusions := createFileExclusions()
-	renaming := createFileRenaming()
+	renames := createFileRenames()
 
 	fmt.Println("Generating files...")
 	err := fs.WalkDir(templateFiles, ".", func(path string, d fs.DirEntry, err error) error {
@@ -61,8 +62,6 @@ func Generate(cfg *GeneratorConfig) error {
 		if shouldExcludeFile(path, project, exclusions) {
 			return nil
 		}
-
-		targetPath = getTargetPath(path, targetPath, project, renaming)
 
 		if d.IsDir() {
 			return os.MkdirAll(targetPath, 0777)
@@ -84,9 +83,19 @@ func Generate(cfg *GeneratorConfig) error {
 		}
 
 		newFilePath := strings.TrimSuffix(targetPath, ".templ")
+
+		targetDir := filepath.Dir(newFilePath)
+		if err := os.MkdirAll(targetDir, 0777); err != nil {
+			return fmt.Errorf("failed to create directory %s: %v", targetDir, err)
+		}
+
 		return os.WriteFile(newFilePath, buf.Bytes(), 0777)
 	})
 	if err != nil {
+		return err
+	}
+
+	if err := processFileRenames(project, outputPath, renames); err != nil {
 		return err
 	}
 
@@ -134,15 +143,100 @@ func createFileExclusions() *FileExclusions {
 	}
 }
 
-func createFileRenaming() *FileRenaming {
-	return &FileRenaming{
+func createFileRenames() *FileRenames {
+	return &FileRenames{
 		ByAppType: map[AppType]map[string]string{
 			Web: {
-				"/cmd/api/": "/cmd/web/",
 				"/cmd/api/main.go": "/cmd/web/main.go",
 			},
 		},
 	}
+}
+
+func processFileRenames(project *Project, outputPath string, renames *FileRenames) error {
+	if renameMappings, ok := renames.ByAppType[project.AppType]; ok {
+		// Track directories that might need cleanup
+		dirsToCheck := make(map[string]bool)
+
+		// Process all file renames
+		for oldPath, newPath := range renameMappings {
+			if err := renameFile(outputPath, oldPath, newPath); err != nil {
+				return err
+			}
+
+			// Add source directory to cleanup list
+			sourceDir := filepath.Dir(filepath.Join(outputPath, oldPath))
+			dirsToCheck[sourceDir] = true
+		}
+
+		// Clean up empty directories
+		if err := cleanupEmptyDirs(dirsToCheck); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func renameFile(basePath, oldRelPath, newRelPath string) error {
+	fullOldPath := filepath.Join(basePath, oldRelPath)
+	fullNewPath := filepath.Join(basePath, newRelPath)
+
+	if _, err := os.Stat(fullOldPath); os.IsNotExist(err) {
+		return nil
+	}
+
+	targetDir := filepath.Dir(fullNewPath)
+	if err := os.MkdirAll(targetDir, 0777); err != nil {
+		return fmt.Errorf("failed to create directory %s: %v", targetDir, err)
+	}
+
+	data, err := os.ReadFile(fullOldPath)
+	if err != nil {
+		return fmt.Errorf("failed to read file %s: %v", fullOldPath, err)
+	}
+
+	if err := os.WriteFile(fullNewPath, data, 0666); err != nil {
+		return fmt.Errorf("failed to write file %s: %v", fullNewPath, err)
+	}
+
+	if err := os.Remove(fullOldPath); err != nil {
+		return fmt.Errorf("failed to remove file %s: %v", fullOldPath, err)
+	}
+
+	return nil
+}
+
+func cleanupEmptyDirs(dirs map[string]bool) error {
+	var dirList []string
+	for dir := range dirs {
+		dirList = append(dirList, dir)
+	}
+
+	sort.Slice(dirList, func(i, j int) bool {
+		return len(dirList[i]) > len(dirList[j])
+	})
+
+	for _, dir := range dirList {
+		// Check if directory exists
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			continue
+		}
+
+		// Check if directory is empty
+		entries, err := os.ReadDir(dir)
+		if err != nil {
+			return fmt.Errorf("failed to read directory %s: %v", dir, err)
+		}
+
+		// Remove if empty
+		if len(entries) == 0 {
+			if err := os.Remove(dir); err != nil {
+				return fmt.Errorf("failed to remove empty directory %s: %v", dir, err)
+			}
+		}
+	}
+
+	return nil
 }
 
 func shouldExcludeFile(path string, project *Project, exclusions *FileExclusions) bool {
@@ -155,18 +249,6 @@ func shouldExcludeFile(path string, project *Project, exclusions *FileExclusions
 	}
 
 	return false
-}
-
-func getTargetPath(srcPath, originalTargetPath string, project *Project, renaming *FileRenaming) string {
-	if renamingRules, ok := renaming.ByAppType[project.AppType]; ok {
-		targetPath := originalTargetPath
-		for pattern, replacement := range renamingRules {
-			if strings.Contains(srcPath, pattern) {
-				return strings.Replace(targetPath, pattern, replacement, 1)
-			}
-		}
-	}
-	return originalTargetPath
 }
 
 func runPostCommands(project *Project, outputPath string) error {
