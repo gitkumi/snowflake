60e651b3e0ee69792f00a133d4bc67e6f07f4251
Takumi Ito <ito.tkmi@gmail.com>
2025-04-25 01:01:02 +0900

    wip

--- internal/initialize/files.go
+++ internal/initialize/files.go
@@ -9,13 +9,7 @@ import (
 )
 
 type FileExclusions struct {
-	SMTP          []string
-	Storage       []string
-	Redis         []string
-	AppType       map[AppType][]string
-	Database      map[Database][]string
-	BackgroundJob map[BackgroundJob][]string
-	ExcludeFuncs  []*ExcludeFunc
+	ExcludeFuncs []*ExcludeFunc
 }
 
 type ExcludeFunc struct {
@@ -29,61 +23,79 @@ type FileRenames struct {
 
 func NewFileExclusions() *FileExclusions {
 	return &FileExclusions{
-		SMTP: []string{
-			"/internal/smtp/mailer.go",
-			"/internal/smtp/mailer_smtp.go",
-			"/internal/smtp/mailer_mock.go",
-		},
-		Storage: []string{
-			"/internal/storage/storage.go",
-			"/internal/storage/storage_s3.go",
-			"/internal/storage/storage_mock.go",
-		},
-		Redis: []string{
-			"/internal/middleware/rate_limit.go",
-		},
-		AppType: map[AppType][]string{
-			AppTypeAPI: {
-				"/internal/html/hello.templ",
-				"/internal/application/handler/html_handler.go",
-			},
-		},
-		Database: map[Database][]string{
-			DatabaseNone: {
-				"/sqlc.yaml",
-				"/dev.yaml",
-				"/static/sql/migrations/00001_books.sql",
-				"/static/sql/queries/books.sql",
-				"/static/static.go",
-				"/internal/application/db.go",
-				"/test/fixtures.go",
-				"/internal/application/handler/book_handler.go",
-				"/internal/application/handler/book_handler_test.go",
-				"/internal/application/service/book_service.go",
-			},
-		},
-		BackgroundJob: map[BackgroundJob][]string{
-			BackgroundJobBasic: {
-				"/internal/queue/queue.go",
-				"/internal/queue/queue_sqs.go",
-				"/internal/queue/queue_mock.go",
-			},
-			BackgroundJobSQS: {
-				"/internal/application/task.go",
-			},
-			BackgroundJobNone: {
-				"/internal/application/task.go",
-				"/internal/queue/queue.go",
-				"/internal/queue/queue_sqs.go",
-				"/internal/queue/queue_mock.go",
-			},
-		},
 		ExcludeFuncs: []*ExcludeFunc{
 			{
 				FilePaths: []string{"/dev.yaml"},
 				Check: func(p *Project) bool {
 					return p.Database == DatabaseSQLite3 && !p.Redis
 				},
+			},
+			{
+				FilePaths: []string{
+					"/internal/smtp/mailer.go",
+					"/internal/smtp/mailer_smtp.go",
+					"/internal/smtp/mailer_mock.go",
+				},
+				Check: func(p *Project) bool { return !p.SMTP },
+			},
+			{
+				FilePaths: []string{
+					"/internal/storage/storage.go",
+					"/internal/storage/storage_s3.go",
+					"/internal/storage/storage_mock.go",
+				},
+				Check: func(p *Project) bool { return !p.Storage },
+			},
+			{
+				FilePaths: []string{
+					"/internal/middleware/rate_limit.go",
+				},
+				Check: func(p *Project) bool { return !p.Redis },
+			},
+			{
+				FilePaths: []string{
+					"/internal/html/hello.templ",
+					"/internal/application/handler/html_handler.go",
+				},
+				Check: func(p *Project) bool { return p.AppType == AppTypeAPI },
+			},
+			{
+				FilePaths: []string{
+					"/sqlc.yaml",
+					"/dev.yaml",
+					"/static/sql/migrations/00001_books.sql",
+					"/static/sql/queries/books.sql",
+					"/static/static.go",
+					"/internal/application/db.go",
+					"/test/fixtures.go",
+					"/internal/application/handler/book_handler.go",
+					"/internal/application/handler/book_handler_test.go",
+					"/internal/application/service/book_service.go",
+				},
+				Check: func(p *Project) bool { return p.Database == DatabaseNone },
+			},
+			{
+				FilePaths: []string{
+					"/internal/queue/queue.go",
+					"/internal/queue/queue_sqs.go",
+					"/internal/queue/queue_mock.go",
+				},
+				Check: func(p *Project) bool { return p.BackgroundJob == BackgroundJobBasic },
+			},
+			{
+				FilePaths: []string{
+					"/internal/application/task.go",
+				},
+				Check: func(p *Project) bool { return p.BackgroundJob == BackgroundJobSQS },
+			},
+			{
+				FilePaths: []string{
+					"/internal/application/task.go",
+					"/internal/queue/queue.go",
+					"/internal/queue/queue_sqs.go",
+					"/internal/queue/queue_mock.go",
+				},
+				Check: func(p *Project) bool { return p.BackgroundJob == BackgroundJobNone },
 			},
 		},
 	}
@@ -105,49 +117,6 @@ func ExcludeTemplateFile(templateFileName string, project *Project, exclusions *
 	for _, fn := range exclusions.ExcludeFuncs {
 		for _, filePath := range fn.FilePaths {
 			if fileName == filePath && fn.Check(project) {
-				return true
-			}
-		}
-	}
-
-	featureExclusions := []struct {
-		enabled  bool
-		pathList []string
-	}{
-		{project.Redis, exclusions.Redis},
-		{project.SMTP, exclusions.SMTP},
-		{project.Storage, exclusions.Storage},
-	}
-
-	for _, feature := range featureExclusions {
-		if !feature.enabled {
-			for _, path := range feature.pathList {
-				if fileName == path {
-					return true
-				}
-			}
-		}
-	}
-
-	if paths, ok := exclusions.AppType[project.AppType]; ok {
-		for _, path := range paths {
-			if fileName == path {
-				return true
-			}
-		}
-	}
-
-	if paths, ok := exclusions.Database[project.Database]; ok {
-		for _, path := range paths {
-			if fileName == path {
-				return true
-			}
-		}
-	}
-
-	if paths, ok := exclusions.BackgroundJob[project.BackgroundJob]; ok {
-		for _, path := range paths {
-			if fileName == path {
 				return true
 			}
 		}
